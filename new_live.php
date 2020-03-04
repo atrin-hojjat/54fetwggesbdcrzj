bb5476dde7b7787082043944b52735d39528d2d9
Atrin Hojjat <atrin.hojjat@gmail.com>
2020-03-05 01:36:17 +0330

    Fix Bugs

--- new_live.php
+++ new_live.php
@@ -7,22 +7,20 @@ echo hash("sha256", $_POST['password']);
 if($_SERVER["REQUEST_METHOD"] == "POST") {
 	if(hash("sha256", $_POST['password']) == "a87a5843b906488216ff1614dee5b5b3a59470163617f462061f9cba863b535b")
 	{
-		echo "Enter<br>";
-		$prep = $conn->prepare("INSERT INOT puz_liveshows(name, date) values(?, ?)");
-		echo 'done<br>';
+		$prep = $conn->prepare("INSERT INTO puz_liveshows(name, date) VALUES(?, ?)");
 		$prep->bind_param("ss", $_POST['name'], $_POST['date']);
+		$prep->execute();
 		echo "DONE 1<br>";
-		$prep.execute();
-		echo "DONE 1<br>";
+		$prep->close();
 		$sql_2 = "SELECT idcode FROM puz_users";
 		$res = mysqli_query($conn, $sql_2);
 		if (mysqli_num_rows($result) > 0) {
 	    while($row = mysqli_fetch_assoc($result)) {
 				$prep_2 = $conn->prepare("INSERT INTO puz_absense(idcode, absense, time, livename) values(?, ?, ?, ?)");
-				$prep.bind_param("s", $row['idcode']);
-				$prep.bind_param("i", 0);
-				$prep.bind_param("i", 0);
-				$prep.bind_param("s", $_POST['name']);
+				$prep->bind_param("s", $row['idcode']);
+				$prep->bind_param("i", 0);
+				$prep->bind_param("i", 0);
+				$prep->bind_param("s", $_POST['name']);
 			}
 		}
 
