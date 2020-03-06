44c62403fb8c0c2744f05374956d2ad755260168
Atrin Hojjat <atrin.hojjat@gmail.com>
2020-03-06 14:27:23 +0330

    Check last login time before updating time_online

--- continue_session.php
+++ continue_session.php
@@ -2,14 +2,18 @@
 include("config.php");
 ?>
 <?php
-$prep = $conn->prepare("SELECT `absense`, `time` from `puz_absense` where `idcode`=? and `livename`=?");
+$prep = $conn->prepare("SELECT `absense`, `time`, `last_check` from `puz_absense` where `idcode`=? and `livename`=?");
 $prep->bind_param("ss", $_SESSION['idcode'], $_SESSION['livegroup']);
 $prep->execute();
 $res = $prep->get_result();
 $now = date("Y-M-D H:i:s");
+$last_10_mins = date("Y-M-D H:i:s", strtotime("-8 minutes"));
 while($row = $res->fetch_assoc()) {
 	$prep_2 = $conn->prepare("UPDATE `puz_absense` SET `absense`=?, `time`=?, `last_check`=? WHERE `idcode`=? and `livename`=?");
-	$new_time = $row['time'] + 1;
+	$new_time = $row['time'];
+	if($last_10_mins >= $row['last_check'])
+		$new_time += 1;
+	else $now = $row['last_check'];
 	$_SESSION['absense'] = 1;
 	$prep_2->bind_param("iisss", $_SESSION['absense'], $new_time, $now, $_SESSION['idcode'], $_SESSION['livegroup']);
 	$prep_2->execute();
